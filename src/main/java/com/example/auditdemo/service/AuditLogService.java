aa0859533419dbc2638e7a20627b808dbf65af41
Coder <coder@example.com>
2025-12-26 14:36:16 +0530

    Added audit-logs end points for fetching responses

--- src/main/java/com/example/auditdemo/service/AuditLogService.java
+++ src/main/java/com/example/auditdemo/service/AuditLogService.java
@@ -3,9 +3,12 @@ package com.example.auditdemo.service;
 import com.example.auditdemo.model.AuditLog;
 import com.example.auditdemo.repository.AuditLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -30,6 +33,29 @@ public class AuditLogService {
     //get all audit logs
     public List<AuditLog> getAllAuditLogs(){
         return auditlogrepository.findAll();
+    }
+    //get audit log filtering actions
+    public List<AuditLog> getByAction(String action) {
+        return auditlogrepository.findByAction(action);
+    }
+
+    public List<AuditLog> getByUsername(String username)
+    {
+        return auditlogrepository.findByUsername(username);
+    }
+    public List<AuditLog> getLatestLogs(){
+        return auditlogrepository.findTop5ByOrderByTimestampDesc();
+    }
+
+    public List<AuditLog> getAuditLogsByDate(String date)
+    {
+        LocalDate localDate=LocalDate.parse(date);
+        LocalDateTime startOfDay=localDate.atStartOfDay();
+        LocalDateTime endOfDay=localDate.atTime(LocalTime.MAX);
+
+        return auditlogrepository.findByTimestampBetween(startOfDay, endOfDay);
+
+
     }
 
 }
